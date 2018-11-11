cbd460a475c8fad3f6ead21734186e9fb9a0339f
Sameeha Rahman <rahman.sameeha@gmail.com>
2018-11-11 12:37:27 +0530

    Catch errors in request routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,73 +1,99 @@
-'use strict'
+"use strict";
 
-var cors = require('cors');
-const express = require('express');
+var cors = require("cors");
+const express = require("express");
 const userRoutes = express.Router();
-const db = require('../dbConnection');
+const db = require("../dbConnection");
 const User = db.User;
-const bcrypt = require('bcryptjs');
-userRoutes.all('*', cors());
+const bcrypt = require("bcryptjs");
+userRoutes.all("*", cors());
 
-userRoutes.post('/all', function (req, res) {
+userRoutes.post("/all", function(req, res) {
+  try {
     let currentUser = req.body.username;
-    User.find({}, function (error, users) {
+    if (currentUser) {
+      User.find({}, function(error, users) {
         let userList = [];
-
         if (users === null) {
-            res.status(212).send("No other users.");
+          res.status(212).send("No other users.");
         } else {
-            users.forEach(user => {
-                if (user.username !== currentUser) {
-                    userList.push(user);
-                }
-            });
-            if (userList.length !== 0) {
-                res.status(200).send(userList);
+          users.forEach(user => {
+            if (user.username !== currentUser) {
+              userList.push(user.username);
             }
+          });
+          if (userList.length !== 0) {
+            res.status(200).send(userList);
+          }
         }
-    })
-})
+      });
+    } else {
+      res.status(212).send("Incomplete data");
+    }
+  } catch (err) {
+    res.status(212).send("An error occured. Please try again.");
+  }
+});
 
 // register a new user
-userRoutes.post('/register', function (req, res) {
+userRoutes.post("/register", function(req, res) {
+  try {
     var username = req.body.username;
-    User.findOne({ username }, function (error, user) {
+    var firstName = req.body.firstName;
+    var password = req.body.password;
+    var lastName = req.body.lastName;
+    if (firstName && lastName && username && password) {
+      User.findOne({ username }, function(error, user) {
         if (user) {
-            res.status(212).send("This user name has already been taken");
+          res.status(212).send("This user name has already been taken");
         } else {
-            var hash = bcrypt.hashSync(req.body.password, 10);
-            User.create(
-                {
-                    username: req.body.username,
-                    hash: hash,
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    createdDate: new Date()
-                },
-                function (error, user) {
-                    if (error) {
-                        res.status(212).send(error)
-                    }
-                    res.status(200).send("New user registered successfully")
-                }
-            )
+          var hash = bcrypt.hashSync(password, 10);
+          User.create(
+            {
+              username,
+              hash,
+              firstName,
+              lastName,
+              createdDate: new Date()
+            },
+            function(error, user) {
+              if (error) {
+                res.status(212).send(error);
+              }
+              res.status(200).send("New user registered successfully");
+            }
+          );
         }
-    });
-})
+      });
+    } else {
+      res.status(212).send("Incomplete credentials");
+    }
+  } catch (err) {
+    res.status(212).send("An error occured. Please try again.");
+  }
+});
 
 // perform update on todo item
-userRoutes.post('/signin', function (req, res) {
+userRoutes.post("/signin", function(req, res) {
+  try {
     var username = req.body.username;
     var password = req.body.password;
-    User.findOne({ username }, function (error, user) {
+    if (username && password) {
+      User.findOne({ username }, function(error, user) {
         if (user === null) {
-            res.status(212).send("This user is not registered");
+          res.status(212).send("This user is not registered");
         } else if (user && bcrypt.compareSync(password, user.hash)) {
-            res.status(200).send("User logged in");
+          res.status(200).send("User logged in");
         } else {
-            res.status(212).send("Password or Email is incorrect");
+          res.status(212).send("Email or Password is incorrect");
         }
-    });
-})
+      });
+    } else {
+      res.status(212).send("Incomplete credentials");
+    }
+  } catch (err) {
+    res.status(212).send("An error occured. Please try again.");
+  }
+});
 
-module.exports = userRoutes
+module.exports = userRoutes;
