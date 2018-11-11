117aff92a8949f8f867ae4e17d798c66b059c3a5
Sameeha Rahman <rahman.sameeha@gmail.com>
2018-11-11 12:36:32 +0530

    Configure testing environment

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   DB: "mongodb://127.0.0.1:27017/chatapp",
+  TEST_DB: "mongodb://127.0.0.1:27017/chatapp_test",
   APP_PORT: 4000
 }
--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,16 +1,23 @@
-const config = require('./config.js');
-const mongoose = require('mongoose');
+const config = require("./config.js");
+const mongoose = require("mongoose");
 
-mongoose.connect(config.DB)
-    .then(() => {
-        console.log('Database connection successful')
-    })
-    .catch(err => {
-        console.error('Database connection error')
-    })
+let database = config.DB;
+
+if (process.env.NODE_ENV === "test") {
+  database = config.TEST_DB;
+}
+
+mongoose
+  .connect(database)
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch(err => {
+    console.error("Database connection error");
+  });
 
 mongoose.Promise = global.Promise;
 
 module.exports = {
-    User: require('./models/User')
+  User: require("./models/User")
 };
